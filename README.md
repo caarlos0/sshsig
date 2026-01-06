51a197d1df64e5bae732a71213cfa4b743f9692e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-01-05 22:05:01 -0300

    feat: import sshsig pkg
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# sshsig
+
+This package allows you to use the unmerged [SSHSIG support](https://github.com/golang/crypto/pull/316) in x/crypto/ssh.
---
+++ testdata_test.go
@@ -0,0 +1,31 @@
+package ssh
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/testdata"
+)
+
+var (
+	testPrivateKeys map[string]interface{}
+	testSigners     map[string]ssh.Signer
+)
+
+func init() {
+	var err error
+
+	n := len(testdata.PEMBytes)
+	testPrivateKeys = make(map[string]interface{}, n)
+	testSigners = make(map[string]ssh.Signer, n)
+	for t, k := range testdata.PEMBytes {
+		testPrivateKeys[t], err = ssh.ParseRawPrivateKey(k)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to parse test key %s: %v", t, err))
+		}
+		testSigners[t], err = ssh.NewSignerFromKey(testPrivateKeys[t])
+		if err != nil {
+			panic(fmt.Sprintf("Unable to create signer for test key %s: %v", t, err))
+		}
+	}
+}
